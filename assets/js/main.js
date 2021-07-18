6c294f39c7d83531605393ff70b06cfed621cc74
Handersen Adriano <handersen.adry@gmail.com>
2021-07-18 14:00:54 -0300

    criação e estilização da seção de testemunhos

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,7 +93,7 @@ modalCloses.forEach((modalClose) => {
     });
 });
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
     navigation: {
@@ -107,7 +107,22 @@ let swiper = new Swiper('.portfolio__container', {
 })
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grapCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+})
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
