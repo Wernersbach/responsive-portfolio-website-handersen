b3c2e2f4d891d48c9ea32f232da1b755effa1a89
Handersen Adriano <handersen.adry@gmail.com>
2021-07-22 00:03:21 -0300

    final do darktheme

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,31 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+//tópico selecionado anteriormente (se o usuário selecionou)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+//obtemos o tema atual que a interface possui validando a classe dark-theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+//validamos se o usuário escolheu previamente um tópico
+if (selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+//Ativar e desativar o tema maualmente com o botao
+themeButton.addEventListener('click', () => {
+    //adiciona ou remove o dark/icon botao
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    //nos salvamos o tema e o icone atual que o usuario ecolheu
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-theme', getCurrentIcon());
+});
